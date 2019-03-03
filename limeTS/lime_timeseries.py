0af81f3c13fd07c1de41a7858925ecf30e89d375
isa-guillou <isabelle.guillou@insa-rennes.fr>
2019-03-03 15:57:12 +0100

    avancement class IndexedTS

--- limeTS/lime_timeseries.py
+++ limeTS/lime_timeseries.py
@@ -1,12 +1,12 @@
 """
 Functions for explaining time series classifiers.
 """
-
+import itertools
 import numpy as np
 import scipy as sp
 import sklearn
 from sklearn.utils import check_random_state
-
+"""
 from . import explanation
 from . import lime_base
 
@@ -25,36 +25,36 @@ class TSDomainMapper(explanation.DomainMapper):
         return exp
 
     def visualize_instance_html(ts):
+        return 0
 
-
-
-
+"""
 class IndexedTS(object):
 
     def __init__(self, raw_ts, bow=True):
-         """Initializer.
-
-        Args:
-            raw_ts: time series with raw time series in it
-            bow: if True, a sub-time-series is the same everywhere in the time series
-            - i.e. we will index multiple occurrences of the same sub-time-series.
-            If False, order matters, so that the same sub-time-series will have
-            different ids according to position.
-        """
         self.raw = raw_ts
         self.as_list = list(self.raw)
-        self.inverse_vocab = []
         self.as_np = np.array(self.as_list)
         self.timeseries_start = np.arange(len(self.raw))
-        vocab = {}
-        self.inverse_vocab = []
+        values = {}
+        self.inverse_values = []
         self.positions = []
         self.bow = bow
-        non_vocab = set()
-
-
-
-
+        non_values = set()
+        for i, value in enumerate(self.as_np):
+            if value in non_values:
+                continue
+            if bow:
+                if value not in values:
+                    values[value] = len(values)
+                    self.inverse_values.append(value)
+                    self.positions.append([])
+                idx_value = values[value]
+                self.positions[idx_value].append(i)
+            else:
+                self.inverse_value.append(value)
+                self.positions.append(i)
+        if not bow:
+            self.positions = np.array(self.positions)
 
 
     def raw_timeSeries(self):
@@ -62,61 +62,58 @@ class IndexedTS(object):
         return self.raw
 
     def num_timeSubSeries(self):
-        """Returns the number of tokens in the vocabulary for this document."""
-        return len(self.inverse_vocab)
+        """Returns the number of different values in the time series."""
+        return len(self.inverse_values)
 
     def timeSubSeries(self, id_):
         """Returns the sub-time-series that corresponds to id_ (int)"""
-        return self.inverse_vocab[id_]
+        return self.inverse_values[id_]
    
     def timeSeries_position(self, id_):
-         """Returns a np array with indices to id_ (int) occurrences"""
         if self.bow:
             return self.timeseries_start[self.positions[id_]]
         else:
             return self.timeseries_start[[self.positions[id_]]]
 
-
     def tsSegmentation(self, seg_length=5, segmentationType=None):
-        """ Fait-on plusieurs types de segmentation?"""
         res = [self.raw[x:x+seg_length] for x in range(0,len(self.raw),seg_length)]
         return res
 
-    def inverse_removing(self, sub_ts_to_remove):
-        """Returns a time series after removing the appropriate sub-time-series.
-
+    def inverse_removing(self, values_to_remove):
+        """Returns a time series after removing the appropriate values.
         If self.bow is false, replaces sub-time-series with UNKWNOW_TS instead of removing
         it.
-
         Args:
-            sub_ts_to_remove: list of ids (ints) to remove
-
+            values_to_remove: list of ids (ints) to remove
         Returns:
-            original raw time series with appropriate sub-time-series removed.
+            original raw time series with appropriate values removed.
         """
         mask = np.ones(self.as_np.shape[0], dtype='bool')
-        mask[self.__get_idxs(sub_ts_to_remove)] = False
+        mask[self.__get_idxs(values_to_remove)] = False
         if not self.bow:
             return ''.join([self.as_list[i] if mask[i]
                             else chr(0) for i in range(mask.shape[0])])
         return ''.join([self.as_list[v] for v in mask.nonzero()[0]])
 
-        def __get_idxs(self, timeSubSeries):
-        """Returns indexes to appropriate timeSubSeries."""
+    def __get_idxs(self, values):
+        """Returns indexes to appropriate values."""
         if self.bow:
-            return list(itertools.chain.from_iterable(
-                [self.positions[z] for z in timeSubSeries]))
+            return list(for i in range(len(values)):
+                [self.positions[z] for z in values])))
         else:
-            return self.positions[timeSubSeries]
+            return self.positions[values]
 
 
 class TSExplainer(object):
 
     def __init__():
+        return 0
 
-    def explain_instance(tsToExplain, classifier, label, numFeatures, numSamples, distance\_metric):
+    def explain_instance(tsToExplain, classifier, label, numFeatures, numSamples, distance_metric):
+        return 0
 
     def data_labels_distances(index, classifier, numSamples, distance_metric):
+        return 0
 
 """ OTHER USEFULL FUNCTIONS """
 
@@ -129,3 +126,22 @@ def generateTS(size=100,min=0,max=10):
 def generateMockExp(ts):
     res = [(x,uniform(0,1)) for x in range(0,len(ts))]
     return res
+
+
+"""TEST"""
+myTS = [0.0, 0.21, 1.24, 1.21, 0.21 , 0.85, 1.96]
+print (myTS)
+myindexedTS = IndexedTS(myTS)
+mysegts = myindexedTS.tsSegmentation()
+#TS brute
+print (myindexedTS.raw_timeSeries())
+#TS segmentation
+print (mysegts)
+#longueur TS
+print (myindexedTS.num_timeSubSeries())
+#rendre une valeur en fonction de son id 
+print (myindexedTS.timeSubSeries(2))
+#rendre toutes les positions de l'indice passe en param
+print (myindexedTS.timeSeries_position(1))
+#Enlever les mots aux indices donnes
+print (myindexedTS.inverse_removing([0,1]))
