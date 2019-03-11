38c16dfb92058ae901e6129dae4dd52a63e63c81
yocouanon <yoyocouanon@gmail.com>
2019-03-11 22:59:29 +0100

    Résolution pb import fichier TS

--- Classifier/testImporAndClassifier.py
+++ Classifier/testImporAndClassifier.py
@@ -2,25 +2,25 @@ import importTS
 import LearningClassifier
 import numpy
 
-
-
-
 "Fichier de test"
 
-X_train, Y_train, X_test, Y_test= importTS.dataImport("Trace")
-print("Résultat attendu : " +str(Y_test[1]))
+
+X_train, Y_train, X_test, Y_test = importTS.dataImport("Trace")
+print("Résultat attendu : " + str(Y_test[1]))
+
+
 "Test LS"
-clLS  = LearningClassifier.learningShapeletClassifier(X_train,Y_train)
+clLS = LearningClassifier.learningShapeletClassifier(X_train, Y_train)
 print("Résultat LS : " + str(clLS.predict(X_test[1].ravel().tolist())))
 
-LearningClassifier.saveClassifierLS(clLS,"LS")
-loadclLS, labelsave  = LearningClassifier.loadClassifieurLS("LS")
+LearningClassifier.saveClassifierLS(clLS, "LS")
+loadclLS, labelsave = LearningClassifier.loadClassifieurLS("LS")
 newlabel = loadclLS.predict(X_test)
 print("Résulat classifieur LS save : " + str(labelsave.inverse_transform(newlabel)[1]))
 
 
 "Test 1NN"
-cl = LearningClassifier.NN1_DTWClassifier(X_train,Y_train)
+cl = LearningClassifier.NN1_DTWClassifier(X_train, Y_train)
 print("Résultat 1NN : " + str(cl.predict(X_test[1].ravel().tolist())))
 
 LearningClassifier.saveClassifier1NN(cl, "1NN")
@@ -29,3 +29,7 @@ loadcl = LearningClassifier.loadClassifieur1NN("1NN")
 print("Résulat classifieur 1NN save : " + str(loadcl.predict(X_test[1].ravel().tolist())))
 
 
+"Test import file"
+tsx, tsy = importTS.fileImportTS("TimeSeriesFiles/myTS.txt")
+print(tsx)
+print(tsy)
