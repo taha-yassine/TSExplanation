c1a98766ea5c6c7b086a90fefff80e086962beba
isa-guillou <isabelle.guillou@insa-rennes.fr>
2019-03-18 20:58:32 +0100

    avancement TSExplainer

--- limeTS/test_LimeShape.py
+++ limeTS/test_LimeShape.py
@@ -9,7 +9,7 @@ import lime_timeseries
 X_train et Y_train servent à l'entrainement du classifieur. Les ST non étiqueté sont dans X_test."""
 X_train, Y_train, X_test, Y_test = importTS.dataImport("Trace")
 
-"""Construction classifieur 1NN-DTW. En vraie, on va le loader"""
+"""Construction classifieur 1NN-DTW. En vrai, on va le loader"""
 cl = LearningClassifier.NN1_DTWClassifier(X_train, Y_train)
 
 
@@ -35,6 +35,6 @@ print ("enleve mots indice 0 et 1:", myindexedTS.inverse_removing([0,1]))
 "Pour indexed ça a l'air de marcher"
 
 """Pour TSexplainer et utiliser le classifier, faudra que l'on code la fonction data_label_distance
-car c'est celle la qui utilise le classifier. Mais à mon avis yaura juste à le passer en paramètre comme ca :
-
-data, labels, distances = data_labels_distances(self, myTs, cl, 2, cosine)"""
+car c'est celle la qui utilise le classifier. Mais à mon avis yaura juste à le passer en paramètre comme ca :"""
+myTSexp=lime_timeseries.TSExplainer(myindexedTS.raw_timeSeries())
+data, labels, distances = myTSexp.data_labels_distances(myindexedTS, cl, 2)
