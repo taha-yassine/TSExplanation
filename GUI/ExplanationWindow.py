912f5ad5d3f086eef1c1b30c9c1900771f48db91
Lisa Relion <lisa.relion@insa-rennes.fr>
2019-04-15 15:44:06 +0200

    MAJ ExplanationWindow

--- GUI/ExplanationWindow.py
+++ GUI/ExplanationWindow.py
@@ -1,65 +1,77 @@
 import sys
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QDialog, QApplication, QPushButton, QVBoxLayout, QWidget
+sys.path.insert(0, "../limeTS")
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtWidgets import QDialog, QApplication, QPushButton, QVBoxLayout, QWidget, QFileDialog, QScrollArea
+from PyQt5.QtCore import Qt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 import matplotlib.pyplot as plt
-import random
+#import explanation
 
 class UI_Explanation(QWidget):
+
+    explanation = None
 
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Explanation')
         self.setGeometry(400, 200, 400, 331) 
-        #self.setStyleSheet("""background: white;""")
 
-        # a figure instance to plot on
-
-        self.lbl1 = QtWidgets.QLabel("Résultat : classe1")
+        #self.lbl_class = QtWidgets.QLabel("Résultat : classe1")
+        self.lbl_class = QtWidgets.QLabel("")
         self.figure = plt.figure()
         self.canvas = FigureCanvas(self.figure)
-        self.button = QPushButton('Sauvegarder')
-        self.lbl2 = QtWidgets.QLabel("Poids : (ss8, 0.5), (ss1, 0.5), (ss3, 0.3), (ss6, 0.2), (ss2, 0.05)... ")
+        #self.lbl_weights = QtWidgets.QLabel("Poids : (ss8, 0.5), (ss1, 0.5), (ss3, 0.3), (ss6, 0.2), (ss2, 0.05)... ")
+        self.lbl_weights = QtWidgets.QLabel("")
+        self.scroll = QtWidgets.QScrollArea()
+        self.scroll.setWidget(self.lbl_weights)
+        self.scroll.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        self.scroll.setWidgetResizable(True)
+        self.scroll.setFixedHeight(50)
+        self.btn_save = QPushButton('Sauvegarder')
+        self.btn_save.clicked.connect(self.saveExplanation)
 
-        data = [random.random() for i in range(11)]
-        self.figure.clear()
-        ax = self.figure.add_subplot(111)
-        ax.plot(data, '*-')
-        self.canvas.draw()
-
-        self.button.clicked.connect(self.plot)
-
-        # set the layout
         layout = QVBoxLayout()
-        layout.addWidget(self.lbl1)
+        layout.addWidget(self.lbl_class)
         layout.addWidget(self.canvas)
-        layout.addWidget(self.lbl2)
-        layout.addWidget(self.button)
+        layout.addWidget(self.scroll)
+        layout.addWidget(self.btn_save)
         self.setLayout(layout)
 
-    # Action of the button 'Sauvegarder'
-    #def saveExplanation(self):
 
+    # Action of the button 'Sauvegarder'
+    def saveExplanation(self):
+        fileName, _  = QFileDialog.getSaveFileName(self, "Sauvegarder un fichier","../","HTML (*.html)") # Créer dossier savedExplanations ??
+        #if fileName:
+        #    UI_Explanation.explanation.save_to_file(fileName)
+
+
+    # Show the plot
     def plot(self):
-        ''' plot some random stuff '''
-        # random data
-        data = [random.random() for i in range(10)]
-        # instead of ax.hold(False)
+        ts = [-0.34086,-0.38038,-0.3458,-0.36556,-0.3458,-0.36556,-0.3952,-0.38038,-0.38532,-0.3952,-0.38038,-0.35568,-0.34086,-0.32604,-0.2964,-0.2964,-0.33098,-0.30134,-0.30134,-0.3211,-0.28652]
+        #weights = UI_Explanation.explanation.as_list()
+        weights = [("ss1",-0.8), ("ss2",-0.5), ("ss3",-0.1), ("ss4",0.0), ("ss5",0.1), ("ss6",0.3), ("ss7",0.5), ("ss8",0.8)] # List of tuples
+        sorted_weights = sorted(weights, key=lambda tup: tup[1], reverse=True) # Sort from the biggest weight to the lowest
+        self.lbl_weights.setText("Poids : " + (str(sorted_weights))[1:(len(str(sorted_weights))-1)]) # Print the weights
+        #self.figure = UI_Explanation.explanation.as_pyplot_figure()
         self.figure.clear()
-        # create an axis
         ax = self.figure.add_subplot(111)
-        # plot data
-        ax.plot(data, '*-')
-        # refresh canvas
+        ax.plot(ts, linestyle='-')
         self.canvas.draw()
 
-    def showUI(self, explanation):
+
+    # Show the window
+    def showUI(self):#, explanation, result_class):
+        #UI_Explanation.explanation = explanation
+        result_class = "classe1"
+        self.lbl_class.setText("Résultat : " + result_class)
+        self.plot()
         self.show()
 
-'''if __name__ == '__main__':
+
+# Test
+if __name__ == '__main__':
     app = QApplication(sys.argv)
-
     main = UI_Explanation()
-    main.show()
-
-    sys.exit(app.exec_())'''
+    main.showUI()
+    sys.exit(app.exec_())
