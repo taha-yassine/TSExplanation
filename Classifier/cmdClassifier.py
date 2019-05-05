f0916347df4983cd39562a8ada26973d31ee2da7
Mendes Tangi - Mac <tangimds@gmail.com>
2019-05-05 12:45:27 +0200

    update command classifier

--- Classifier/cmdClassifier.py
+++ Classifier/cmdClassifier.py
@@ -1,27 +1,37 @@
 import argparse
 import sys
 
+with open("../GUI/ListeTS.csv","r") as file:
+    listeTS = file.readline().split(";")
+timeSeries = []
+for ts in listeTS:
+    timeSeries.append(ts)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("input_file", type=str, help="TS used to learn the classifier")
+parser.add_argument("input_file", type=str, choices=timeSeries, help="TS used to learn the classifier")
 parser.add_argument("classifier_type", type=str, choices=['1NN','1NN-DTW','LS'], help="classifier's type")
-parser.add_argument("output_name", type=str, help="name of the output file, it will be followed by the <classifier_type>.")
 
-parser.add_argument("-p", "--perso", help="indicate that the TS file <input_file> is on the computer",
-                    action="store_false")
+parser.add_argument("-o","--output", type=str, help="name of the output file, it will be followed by the <classifier_type>.")
+parser.add_argument("-p", "--perso", action="store_true", help="indicate that the TS file <input_file> is on the computer")
 # default = pas dans la ligne de commande
 # const = dans la ligne mais pas renseigné
 
 args = parser.parse_args()
-
+print(args)
 import importTS
 import LearningClassifier
 
-#fileName, _  = QFileDialog.getSaveFileName(self,"Sauvegarder un fichier","../Classifier/SaveClassifierFiles","Saved classifiers (*.sav)")
 if args.perso == True:
     X_train, Y_train = importTS.fileImportTS(args.input_file)
 else:
     X_train, Y_train, _, _= importTS.dataImport(args.input_file)
+
+fileName = "./SaveClassifierFiles/"
+if args.output:
+    fileName += args.output
+else:
+    fileName += args.input_file
+fileName += ".sav"
 
 if args.classifier_type == "LS":
     classifier = LearningClassifier.learningShapeletClassifier(X_train, Y_train)
@@ -33,4 +43,4 @@ else:
     classifier = LearningClassifier.NN1_Classifier(X_train, Y_train)
     LearningClassifier.saveClassifier1NN(classifier,fileName)
 
-print("Classifier saved !")
+print("Classifier saved at "+fileName+" !")
